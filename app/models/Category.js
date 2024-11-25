db254aea0f815988edd9a4cf7883b17782687844
Michael Giesler <michael.giesler1@icloud.com>
2024-11-25 12:37:28 -0700

    created category and question models with html templates

--- app/models/Category.js
+++ app/models/Category.js
@@ -1,5 +1,14 @@
-export class Example {
-  constructor(message) {
-    this.message = message
+export class Category {
+  constructor(data) {
+    this.id = data.id;
+    this.name = this.sanitizeName(data.name);
+  }
+
+  sanitizeName(name) {
+    return name.replace(/^(Entertainment|Science): /, '');
+  }
+
+  get buttonHTMLTemplate() {
+    return `<button onclick="app.CategoriesController.setActiveCategory(${this.id})">${this.name}</button>`;
   }
 }
--- app/models/Question.js
+++ app/models/Question.js
@@ -1,8 +1,17 @@
-
-
-
 export class Question {
   constructor(data) {
+    this.question = data.question;
+    this.answers = [data.correct_answer, ...data.incorrect_answers].sort(() => 0.5 - Math.random());
+    this.correctAnswer = data.correct_answer;
+  }
 
+  get cardHTMLTemplate() {
+    return `
+      <div>
+        <h4>${this.question}</h4>
+        ${this.answers
+        .map(answer => `<button onclick="app.QuestionsController.answerQuestions('${answer}')">${answer}</button>`)
+        .join('')}
+        </div>`;
   }
 }
